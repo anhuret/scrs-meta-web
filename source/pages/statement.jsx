58d54dbc96b46960c4c22c651073132e43b8a309
Ruslan Sendecky <r.sendecky@gmail.com>
2021-08-18 05:02:48 +1000

    Statement page

--- source/pages/statement.jsx
+++ source/pages/statement.jsx
@@ -1,9 +1,23 @@
 import React, { useState } from 'react'
 import { useStore } from '../store/store.js'
-import css from '../style/space.module.css'
+import css from '../style/statement.module.css'
 
 export const Statement = () => {
   const [tip, setTip] = useStore('tip')
 
-  return <section>Hello World</section>
+  return (
+    <section className={css.statement}>
+      <p>Welcome to Southern Cross !!!</p>
+      <br />
+      <br />
+      <p>Secure, robast and reliable Cardano pool</p>
+      <p>Top financial enterprise level security</p>
+      <p>We strive for 24/7 365 operational uptime</p>
+      <p>Redundant, containerised infrastructure</p>
+      <br />
+      <p>It was forged and made possible by experienced DevOps engineers</p>
+      <p>Our goal is to make this pool a profitable home for your ADA</p>
+      <p>Stake with us for your piece of mind and beautiful experience</p>
+    </section>
+  )
 }
